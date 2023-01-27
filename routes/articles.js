8385b5eaaeb226654c9095f1b32c4bd34a69925a
Gail Foad <gail.foad@thenavpeople.co.uk>
2023-01-27 13:02:08 +0000

    slugify

--- routes/articles.js
+++ routes/articles.js
@@ -6,15 +6,10 @@ router.get('/new', (req, res) => {
 	res.render('articles/new', { article: new Article() })
 });
 
-router.get('/:id', async (req, res) => {
-	const article = await Article.findById(req.params.id);
-
-	console.log('req.params.id', req.params.id);
-
-	console.log('req.params', req.params);
+router.get('/:slug', async (req, res) => {
+	const article = await Article.findOne({slug: req.params.slug});
 
 	if (article == null) res.redirect('/');
-	console.log('just before show***', article);
 	res.render('articles/show', { article: article });
 });
 
@@ -27,10 +22,8 @@ router.post('/', async (req, res) => {
 
 	try {
 		article = await article.save();
-		res.redirect(`/articles/${article.id}`)
+		res.redirect(`/articles/${article.slug}`)
 	} catch (e) {
-		console.log('still broken! *********:', e)
-		console.log('article in catch**&&&&&&&', article)
 		res.render('articles/new', {article: article});
 	}
 	
