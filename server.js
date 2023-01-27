ff23b7cd0a90855409efb094bea0641871417e81
Gail Foad <gail.foad@thenavpeople.co.uk>
2023-01-27 12:34:38 +0000

    Read More button

--- server.js
+++ server.js
@@ -18,9 +18,8 @@ app.use(express.urlencoded({ extended: false}));
 
 app.get('/', async (req, res) => {
 
-	const articles = await Article.find();
+	const articles = await Article.find().sort({ createdAt: 'desc'});
 	res.render('articles/index', {articles : articles});
-
 	
 })
 
